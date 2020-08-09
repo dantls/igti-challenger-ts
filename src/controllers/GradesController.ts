c95d92302e74bd9b5fae6ef57332e6ebffe84e31
dantls <dan_tls@hotmail.com>
2020-08-09 10:50:06 -0400

    CRUD Finished

--- src/controllers/GradesController.ts
+++ src/controllers/GradesController.ts
@@ -4,37 +4,97 @@ import path from 'path';
 
 const archive = path.resolve(__dirname,'..','..', 'grades.json');
 
+interface gradesFormat{
+  id: number;
+  student: string;
+  subject: string;
+  type: string;
+  value: number;
+  timestamp:Date;
+}
+
 export default class GradesController{
   async create(request: Request , response: Response){
-    const { student, subject, type ,value }= request.body;
 
-    let data = JSON.parse((await fs.readFile(archive)).toString());
-    
-    const newGrade = {id:data.nextId , ...{student, subject,type, value, timestamp: new Date()} }
-    
-    data.grades.push(newGrade);
+    try {
+      const grade = request.body;
 
-    data = {nextId: data.nextId++, ...data }
+      const data = JSON.parse((await fs.readFile(archive)).toString());
+      
+      data.nextId++;
 
-    await fs.writeFile(archive, JSON.stringify(data, null, 2));
+      data.grades.push({
+        id:data.nextId,
+        timestamp: new Date(),
+        ...grade
+      });
 
-    response.json(data)
+      await fs.writeFile(archive, JSON.stringify(data, null, 2));
+
+      response.json(grade);
+    } catch (error) {
+      response.status(400).json({error: error.message}); 
+    }
+
   }
-
   async update(request: Request , response: Response){
-    const { student, subject, type ,value }= request.body;
-    const { id }= request.params;
+    try {
+      const {id} = request.params;
+      const grade = request.body;
 
-    let data = JSON.parse((await fs.readFile(archive)).toString());
-    
-    const newGrade = {id:data.nextId , ...{student, subject,type, value, timestamp: new Date()} }
-    
-    data.grades.push(newGrade);
+      const data = JSON.parse((await fs.readFile(archive)).toString());
+      const index = data.grades.findIndex((grade: gradesFormat ) => grade.id === Number(id));
 
-    data = {nextId: data.nextId++, ...data }
+      if(!(index>=0)){
+        console.log(index);
+        throw new Error("Grade doesn't found.");
+      }
 
-    await fs.writeFile(archive, JSON.stringify(data, null, 2));
+      data.grades[index] = {...data.grades[index] , ...grade};
 
-    response.json(data)
+      await fs.writeFile(archive, JSON.stringify(data, null, 2));
+
+      response.json(grade);
+    } catch (error) {
+      response.status(400).json({error: "Grade not found."}); 
+    }
   }
+  async delete(request: Request , response: Response){
+    try {
+      const {id} = request.params;
+
+      const data = JSON.parse((await fs.readFile(archive)).toString());
+      console.log(data);
+      const index = data.grades.findIndex((grade: gradesFormat ) => grade.id === Number(id));
+
+      if(!(index>=0)){
+        console.log(index);
+        throw new Error("Grade doesn't found.");
+      }
+
+      data.grades = data.grades.filter((grade:gradesFormat)=>grade.id !== Number(id));
+      
+      await fs.writeFile(archive, JSON.stringify(data, null, 2));
+
+      response.status(200).json({message: "Deleted"});
+    } catch (error) {
+      response.status(400).json({error: "Grade not found."}); 
+    }
+  }
+  async index(request: Request , response: Response){
+    try {
+      const {id} = request.params;
+
+      const data = JSON.parse((await fs.readFile(archive)).toString());
+      const grade = data.grades.find((grade: gradesFormat ) => grade.id === Number(id));
+
+      if(!grade){
+        throw new Error("Grade doesn't found.");
+      }
+      response.json(grade);
+    } catch (error) {
+      response.status(400).json({error: "Grade not found."}); 
+    }
+  }
+  
 }
--- src/routes.grades.ts
+++ src/routes.grades.ts
@@ -7,5 +7,10 @@ const gradesController = new GradesController();
 const routes = express.Router();
 
 routes.post('/', gradesController.create);
+routes.put('/:id', gradesController.update);
+routes.get('/:id', gradesController.index);
+routes.get('/subject', gradesController.index);
+routes.get('/type', gradesController.index);
+routes.delete('/:id', gradesController.delete);
 
 export default routes;
