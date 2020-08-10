9801aab658c82ebf7b4acb41dc4d5d537210a420
dantls <dan_tls@hotmail.com>
2020-08-09 22:08:54 -0400

    Finished Routes

--- src/routes.grades.ts
+++ src/routes.grades.ts
@@ -6,11 +6,14 @@ const gradesController = new GradesController();
 
 const routes = express.Router();
 
-routes.post('/', gradesController.create);
-routes.put('/:id', gradesController.update);
+
 routes.get('/average', gradesController.calcAverage);
 routes.get('/sum', gradesController.calcSum);
-routes.get('/:id', gradesController.index);
+
+routes.post('/', gradesController.create);
+routes.put('/:id', gradesController.update);
 routes.delete('/:id', gradesController.delete);
+routes.get('/:id', gradesController.index);
+
 
 export default routes;
